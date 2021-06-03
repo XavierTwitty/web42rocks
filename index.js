78e9b2742f2f96c6ad9f95874c6d3d458409ec11
XavierTwitty <x.twitty@gmail.com>
2021-06-03 14:00:24 -0400

    wip

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
 const server = express();
+server.use(cors());
+server.use(express.static(path.join(__dirname, "client/build")));
 
 console.log("hey!!");
 console.log(process.env.USER);
@@ -13,6 +18,9 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 console.log("port is->", PORT);
 
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 server.get("/api", (req, res) => {
   res.json({ message: `${process.env.COHORT} ROCKS` });
 
