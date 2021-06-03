555943ef05cd26547d74b6d54a4643674f03979b
XavierTwitty <x.twitty@gmail.com>
2021-06-03 13:16:42 -0400

    wip

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const server = express();
+
 console.log("hey!!");
+console.log(process.env.USER);
+console.log(process.env.PATH);
+
+if (process.env.NODE_ENV === "production") {
+  console.log("this means this code is deployed");
+}
+
+const PORT = process.env.PORT || 5000;
+console.log("port is->", PORT);
+
+server.get("/api", (req, res) => {
+  res.json({ message: `${process.env.COHORT} ROCKS` });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
